044d6f90f32976b5d4d135c2972082e6b16d9b76
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-03-11 11:02:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import webbrowser
 import ipaddress
-
+import socket
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+print(get_ip())
 
 Protocol = 'https://'
 Google = 'www.google.com'
 Bing = 'www.bing.com'
 DuckDuckGo = 'www.duckduckgo.com'
 SearchEngine = [Google, Bing, DuckDuckGo]
-address = 
-print(ipaddress.ip_address(address))
+#address = 
+#print(ipaddress.ip_address(address))
 webbrowser.open_new(f"{Protocol}{SearchEngine[0]}")
