ec2982267532b16e01c99e1732cdba2425aaaf19
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-03-11 11:13:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import webbrowser
 import ipaddress
 import socket
-def get_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.settimeout(0)
-    try:
-        # doesn't even have to be reachable
-        s.connect(('10.254.254.254', 1))
-        IP = s.getsockname()[0]
-    except Exception:
-        IP = '127.0.0.1'
-    finally:
-        s.close()
-    return IP
-print(get_ip())
+try: 
+    hostname = socket.gethostname()    
+    ipv4_address = socket.gethostbyname(hostname)    
+    print(f"Internal IPv4 Address for {hostname}: {ipv4_address}")
+except socket.gaierror:    
+    print("There was an error resolving the hostname.")
+except Exception as e:    
+    print(f"An unexpected error occurred: {e}")
 
 Protocol = 'https://'
 Google = 'www.google.com'
 Bing = 'www.bing.com'
 DuckDuckGo = 'www.duckduckgo.com'
 SearchEngine = [Google, Bing, DuckDuckGo]
-#address = 
-#print(ipaddress.ip_address(address))
+print(ipaddress.ip_address(ipv4_address))
 webbrowser.open_new(f"{Protocol}{SearchEngine[0]}")
