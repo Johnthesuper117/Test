6369e29add6b79c79a5e4dd5e2386a7257faa24c
Johnthesuper117 <195603431+Johnthesuper117@users.noreply.github.com>
2025-03-11 10:34:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import webbrowser
 
 Protocol = 'https://'
+Google = 'www.google.com'
+Bing = 'www.bing.com'
+DuckDuckGo = 'www.duckduckgo.com'
+SearchEngine = [Google, Bing, DuckDuckGo]
 
-webbrowser.open_new("www.google.com")
+webbrowser.open_new(f"{Protocol}{SearchEngine[0]}")
